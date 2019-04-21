741077f39774abf247c77ff74551bf987dfc7d7e
CEODINIZ <38334385+CEODINIZ@users.noreply.github.com>
2019-04-21 20:56:22 -0300

    testes

--- monstros.py
+++ monstros.py
@@ -1,21 +1,24 @@
-#Esqueleto de dicionario com monstros ou viloes para as fases/salas do jogo (podendo haver mais monstros/ sala do que o jogo pois é um teste
-
 M = {
 
-#'nome da sala': { 'monstro 1' : 'poder monstro 1' , 'monstro 2' : 'poder monstro 2' , 'monstro 3' : 'poder monstro 3' , 'monstro 4' : 'poder monstro 4' , 'monstro 5' : 'poder monstro 5' },
 
-    'S1': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
-    'S2': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
-    'S3': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
-    'S3': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
-    'S4': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Inicio': { 'Seguranca' : 10 , 'Sem carteirinha' : 10 , 'Professor' : 100 , 'Limpeza' : 100 , 'Novo aluno' : '100' },
+    'Andar do Professor': { 'Professor' : 100 , 'Sem computador' : 100 , 'Nao fez o EP' : 100 , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Professor': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Biblioteca': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Predio Novo': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Primeiro Andar': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Segundo Andar': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Terceiro Andar': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
+    'Fab Lap': { 'M1' : 'P1' , 'M2' : 'P2' , 'M3' : 'P3' , 'M4' : 'P4' , 'M5' : 'P5' },
 }
 
-print('Escolha uma sala e um monstro: ')
-sala = input('Sala: ')
+
+print('Escolha um cenario e um monstro: ')
+
+cenario = input('Sala: ')
 monstro = input('Monstro: ')
 
-print(M[sala][monstro])
+print(insper_monster[cenario][monstro])
 
 
 #-------------------------------------------- Funcao
@@ -35,14 +38,14 @@ def insper_monster(a,b):
 	}
 
 
-#	sala = input('Sala: ')
+#	sala = input('Cenario: ')
 #	monstro = input('Monstro: ')
 
-	#return M[sala][monstro]
+	#return M[cenario][monstro]
 	return M[a][b]
 
 	
-sala = input('Sala: ')
+cenario = input('Cenario: ')
 monstro = input('Monstro: ')
 
-print(insper_monster(sala, monstro))
+print(insper_monster(cenario, monstro))
